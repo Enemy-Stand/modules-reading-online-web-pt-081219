ca4ffc883f9dc392a623147c369761b37c6204c4
Joseph Sesti <sestijoey63@gmail.com>
2019-09-22 17:26:59 +0000

    Automatically backed up by Learn

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -1,2 +1,15 @@
 require_relative './class_methods_module.rb'
 require_relative './dance_module.rb'
+module Dance
+  def twirl
+    "i'm twirling!"
+  end
+  
+  def jump
+    "Look how high I'm jumping!"
+  end
+  
+  def pirouette
+    "I'm doing a pirouette"
+  end
+end
